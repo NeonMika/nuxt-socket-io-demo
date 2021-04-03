bc95492a44a13a8d5baa539ed2ee6fe8f52f6b75
Markus Weninger <markusw92@yahoo.de>
2021-04-03 16:11:54 +0200

    Added some output

--- serverMiddleware/socket-io-server.js
+++ serverMiddleware/socket-io-server.js
@@ -1,4 +1,5 @@
 // This file is executed once when the server is started
+console.log("Setting up socket.io server ...")
 
 // Setup a socket.io server on port 3001 that has CORS disabled (do not set this to port 3000 as port 3000 is where the nuxt dev server serves your nuxt application)
 const io = require("socket.io")(3001, {
@@ -15,6 +16,8 @@ setInterval(() => {
     i++;
     io.emit("tick", i);
 }, 1000);
+
+console.log("socket.io server now sends 'tick' event with 'tickId' parameter every second ...")
 
 // Since we are a serverMiddleware, we have to return a handler, even if this it does nothing
 export default function (req, res, next) {
